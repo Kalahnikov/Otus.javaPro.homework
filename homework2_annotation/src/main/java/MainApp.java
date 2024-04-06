6da2574b6a97eda8396a291ac253b526a5d59331
Kalashnikov <fenix34rus@yandex.ru>
2024-04-06 21:00:57 +0300

    homework2_annotation2

---
+++ homework2_annotation/src/main/java/MainApp.java
@@ -0,0 +1,7 @@
+import java.lang.reflect.InvocationTargetException;
+
+public class MainApp {
+    public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        RunTest.startTest(Tests.class);
+    }
+}
